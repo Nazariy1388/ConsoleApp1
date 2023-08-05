58651dd09516ee9efc4dcc3fe29fb28a4121b2a6
Nazariy1388 <ahilles13_88@mail.ru>
2023-08-05 15:21:32 +0300

    1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,9 +11,9 @@ namespace ConsoleApp1
 {
     class Warrior
     {
-        public int Health;
-        public int Armor;
-        public int Damage;
+        protected int Health;
+        protected int Armor;
+        protected int Damage;
         public Warrior(int health, int armor, int damage)
         {
             Health = health;
@@ -34,14 +34,14 @@ namespace ConsoleApp1
         public Knight (int health, int armor, int damage ) : base (health, armor, damage) { }
         public void Pray ()
         {
-            Armor += 2;
-            Health += 5;
+            Armor += 1000;
+            Health += 1000;
 
         }
     }
     class Barbarian : Warrior
     {
-        public Barbarian(int health, int armor, int damage, int attackSpeed) : base(health, armor, damage * attackSpeed) { }
+        public Barbarian(int health, int armor, int damage, int attackSpeed = 10) : base(health, armor, damage * attackSpeed) { }
         public void Shout()
         {
             Armor -= 2;
@@ -65,11 +65,18 @@ namespace ConsoleApp1
             knight.TakeDamage(40);
             barbarian.TakeDamage(50);
 
+            knight.Pray();
+            knight.Pray();
+            knight.Pray();
+            barbarian.TakeDamage(30);
+            knight.TakeDamage(30); 
+
             Console.WriteLine("Здоровье рыцаря после атаки: ");
             knight.ShowInfo();
 
             Console.WriteLine("Здоровье варвара после атаки: ");
-            barbarian.ShowInfo();    
+            barbarian.ShowInfo();
+            Console.ReadKey();
 
 
 
