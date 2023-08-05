2164345f7fdb31a28933f2558536e3bb67d166df
Nazariy1388 <ahilles13_88@mail.ru>
2023-08-05 10:37:31 +0300

    1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,76 +9,17 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Guy
+    clsass Warrior
     {
-        public string Name;
-        public int Cash;
-        public void WriteMyInfo()
-        {
-            Console.WriteLine($"{Name} has {Cash} bucsdlkfjfjsks.");
-        }
-        public int GiveCash (int amount)
-        {
-            if (amount<= 0)
-            {
-                Console.WriteLine("It's not valid amount");
-                return 0;
-            }
-            if (amount>Cash)
-            {
-                Console.WriteLine("I don't have enough cach to give you");
-                return 0;
-            }
-            Cash = Cash - amount;
-            return amount;
-        }
-        public void ResieveCash (int amount)
-        {
-            if (amount <= 0)
-            {
-                Console.WriteLine("It's not valid amount");
-            }
-            else
-            {
-                Cash = Cash + amount;
-            }
-        }
-    } 
+        public int Health
+    }
+
     internal class Program
     {
+    }
+        
         static void Main(string[] args)
         {
-            double odds = .75;
-            Random random = new Random();
-            Guy player = new Guy() { Cash = 100, Name = "The player" };
-            Console.WriteLine("Welcome to the casino. The odds are" + odds );
 
-            while (player.Cash > 0)
-            {
-                player.WriteMyInfo();
-                Console.WriteLine("How much do you want to bet: ");
-                string howMuch = Console.ReadLine();
-                if (int.TryParse(howMuch, out int amount))
-                {
-                    int pot = player.GiveCash(amount) * 2;
-                    if (pot > 0)
-                    {
-                        if (random.NextDouble() > odds)
-                        {
-                            int winnings = pot;
-                            Console.WriteLine("You win" + winnings);
-                            player.ResieveCash(winnings);
-                        } else
-                        {
-                            Console.WriteLine("Bad luck, you lose.");
-                        }
-                    }
-                } else
-                {
-                    Console.WriteLine("Plesae enter a valid number. ");
-                }
-            }
-            Console.WriteLine("The house always wins. ");
         }
-    }
 }
